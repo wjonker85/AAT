93bad76bad178305cb14af7c2dec84be85478ec8
unknown <Wilfried@.(none)>
2011-10-04 17:00:59 +0200

    Set view to false

--- src/AATResults.java
+++ src/AATResults.java
@@ -16,24 +16,27 @@ public class AATResults extends PApplet implements Observer {
 
     public AATResults() {
         setVisible(false);
+     }
+
+    public void draw(){
+        display();
     }
 
-    public void display(){
+    public void display() {
         background(255);
-        fill(255,0,0);
-        rect(10,10,10,10);
+        fill(255, 0, 0);
+        rect(10, 10, 10, 10);
     }
 
     public void update(Observable observable, Object o) {
         model = (AATModel) observable;
-                if(o.equals("View changed")) {
-                    if(model.currentView() == AATModel.SINGLE_RESULTS) {
-                        this.setVisible(true);
-                    }
-                    else {
-                        this.setVisible(false);
-                    }
-                }
+        if (o.equals("View changed")) {
+            if (model.currentView() == AATModel.SINGLE_RESULTS) {
+                this.setVisible(true);
+            } else {
+                this.setVisible(false);
+            }
+        }
 
         //To change body of implemented methods use File | Settings | File Templates.
     }
