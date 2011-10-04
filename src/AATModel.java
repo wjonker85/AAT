ac0af70fe18a001a46978b9b57f2470c932a3593
Marcel <test@test.com>
2011-10-04 15:13:05 +0200

    Bestand aatmodel gemaakt

---
+++ src/AATModel.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: marcel
+ * Date: 10/4/11
+ * Time: 3:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AATModel {
+}
