fb82c4b7c71b4c4782770a7a884873f29d908aea
unknown <Wilfried@.(none)>
2011-10-30 19:51:41 +0100

    Nu nog de array met info vullen.

--- src/AATViewResults.java
+++ src/AATViewResults.java
@@ -13,6 +13,8 @@ import java.util.Observer;
 public class AATViewResults extends PApplet implements Observer {
     private AATModel model;
     public int viewWidth, viewHeight;
+    public float[] array1, array2, array3, array4;
+    BoxPlot boxPlot;
 
     public AATViewResults(int viewWidth, int viewHeight) {
         // Tijdelijk, moet uit model komen.
@@ -21,8 +23,23 @@ public class AATViewResults extends PApplet implements Observer {
         //Grote van de AATView scherm.
         this.viewHeight = viewHeight;
         this.viewWidth = viewWidth;
+        boxPlot = new BoxPlot(array1, array2, array3, array4);
 
     }
+
+    public void setup(){
+        size(200,200);
+    }
+
+    public void draw(){
+        rectMode(CORNERS);
+        rect(0,0, 200, 200);
+        boxPlot.drawBoxPlot(0, 0, 1.5f, "Reactie tijd (ms)", "Conditie", "Één", "Twee", "Drie", "Vier");
+    }
+
+
+
+
 
     public void update(Observable observable, Object o) {
         model = (AATModel) observable;
