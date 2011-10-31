77af109a6aaf7150f19dcf2967fab24009c1ec30
unknown <Wilfried@.(none)>
2011-10-31 18:40:45 +0100

    Bestand van commentaar voorzien :)

--- src/DescriptiveStats.java
+++ src/DescriptiveStats.java
@@ -7,6 +7,10 @@ import processing.core.PApplet;
  * Time: 19:23
  * To change this template use File | Settings | File Templates.
  */
+
+/**
+ * Deze class geeft de 5 getallen samenvatting terug voor een willekeurig array met float waarden
+ */
 public class DescriptiveStats extends PApplet {
     float newArray[];
 
@@ -14,6 +18,11 @@ public class DescriptiveStats extends PApplet {
         this.newArray = newArray;
     }
 
+
+    /**
+     * Weinig onduidelijk aan naam van deze functie, geeft gemiddelde van een array terug
+     * @return gemiddelde (float)
+     */
     public float average() {
         float tot = 0;
         for (int n = 0; n < newArray.length; n++) {
@@ -22,27 +31,44 @@ public class DescriptiveStats extends PApplet {
         return tot / newArray.length;
     }
 
-    //Op basis van methode Moore & McCabe / Discissue blijft of je bij oneven aantal gemiddelde neemt, of de eerst waarde na het 50 percentiel zoals dat wel bij q1 en q3 gebeurd.
+    /**
+     * Mediaan wordt berekent op basis van methode Moore & McCabe. Discissue blijft of je bij oneven aantal
+     * gemiddelde neemt, of de eerst waarde na het 50e percentiel zoals dat wel bij q1 en q3 gebeurd. Moore McCabe
+     * kiezen gemiddelde van de twee waarden vanneer het aantal waarneming bestaat uit een oneven aantal.
+     * @return mediaan (float)
+     */
     public float median() {
         newArray = sort(newArray);
         float pos = (newArray.length + 1) / 2f;
-        int posF = floor(pos) - 1; //Array begint bij 0, dus vandaar - 1
-        int posC = ceil(pos) - 1; //Array begint bij 0, dus vandaar - 1
-        return (newArray[posF] + newArray[posC]) / 2; // meest korte manier van schrijven, wanneer posF en posC gelijk zijn is dit irrelevant, wanneer array oneven aantal waarden kent moet er wel gemiddeld worden.
+        int posF = floor(pos) - 1;
+        int posC = ceil(pos) - 1;
+        return (newArray[posF] + newArray[posC]) / 2;
     }
 
+    /**
+     * Eerste kwartiel, is de eerste waarneming na het 25e percentiel.
+     * @return q1 (float)
+     */
     public float q1() {
         newArray = sort(newArray);
         int pos = ceil((newArray.length) * .25f) - 1;
         return newArray[pos];
     }
 
+    /**
+     * Derder kwartiel, is de eerste waarneming na het 75e percentiel.
+     * @return q3 (float)
+     */
     public float q3() {
         newArray = sort(newArray);
         int pos = ceil((newArray.length) * .75f) - 1;
         return newArray[pos];
     }
 
+    /**
+     * minValue is de eerste waarde welke binnen de q1 - 1.5 IKA regel valt.
+     * @return minValue (float)
+     */
     public float minValue() {
         newArray = sort(newArray);
         int posQ1 = ceil((newArray.length) * .25f) - 1;
@@ -61,6 +87,10 @@ public class DescriptiveStats extends PApplet {
         }
     }
 
+    /**
+     * maxValue is de laatste waarde welke binnen de q3 + 1.5 IKA regel valt.
+     * @return
+     */
     public float maxValue() {
         newArray = sort(newArray);
         int posQ1 = ceil((newArray.length) * .25f) - 1;
