6c50c545d07323bdabb34316b7e76a7bd9593d08
marcel <m.zuur@student.utwente.nl>
2014-02-23 22:46:50 +0100

    Editing a questionnaire is almost possible now. Questions can be added and edited. What needs to be done is adding the ability to edit the intro text and adding context menus for removing or moving questions up or down the list. Last step is saving the results to an xml file.

--- src/views/QuestionPanel.java
+++ src/views/QuestionPanel.java
@@ -32,6 +32,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -56,10 +57,12 @@ public class QuestionPanel extends JPanel {
     private Boolean editMode = false;
     private Questionnaire questionnaire;
 
+
     public QuestionPanel(final AATModel model) {
         if (model == null) {
             editMode = true;
         }
+
         //  super(new BoxLayout(this, BoxLayout.Y_AXIS));
         JPanel mainPanel = new JPanel();
         JPanel contentPanel = new JPanel();
@@ -77,6 +80,18 @@ public class QuestionPanel extends JPanel {
         this.setPreferredSize(new Dimension(screen.width / 2, screen.height));
         this.setMaximumSize(new Dimension(screen.width / 2, screen.height));
 
+        if (editMode) {
+            JPanel addButtonPanel = new JPanel();
+            JButton button = new JButton("Add question");
+            addButtonPanel.add(button);
+            contentPanel.add(addButtonPanel);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    addQuestionAction();
+                }
+            });
+        }
 
         questionsPanel = new JPanel(new SpringLayout());
         JPanel submitPanel = new JPanel();
@@ -150,6 +165,15 @@ public class QuestionPanel extends JPanel {
     }
 
 
+    private void addQuestionAction() {
+        if (questionnaire == null) {
+            questionnaire = new Questionnaire(new ArrayList<QuestionData>(), "");
+        }
+        QuestionEditFrame editFrame = new QuestionEditFrame(null, questionnaire.getExtraQuestions().size() + 1, this);
+        editFrame.setEnabled(true);
+        editFrame.setVisible(true);
+    }
+
     private boolean checkAnswered() {
         int count = 0;
         for (String key : questionsMap.keySet()) {
@@ -176,6 +200,7 @@ public class QuestionPanel extends JPanel {
      */
     public void displayQuestions(Questionnaire questionnaire) {
         this.questionnaire = questionnaire;
+        System.out.println("No questions "+questionnaire.getExtraQuestions().size());
         introductionPane.setText(questionnaire.getIntroduction());
         for (int x = 0; x < questionnaire.getExtraQuestions().size(); x++) {
             //for (QuestionData questionObject : questionnaire.getExtraQuestions()) {
@@ -263,8 +288,14 @@ public class QuestionPanel extends JPanel {
 
 
     public void changeQuestion(QuestionData newQuestion, int pos) {
-        questionnaire.getExtraQuestions().set(pos, newQuestion);
+        if (pos >= 0 && pos < questionnaire.getExtraQuestions().size()) {
+            questionnaire.getExtraQuestions().set(pos, newQuestion);
+        } else {
+            System.out.println("Adding question at pos "+pos);
+            questionnaire.getExtraQuestions().add(newQuestion);
+        }
         questionsPanel.removeAll();
+        questionsPanel.setLayout(new SpringLayout());
         //  questionsPanel = new JPanel(new SpringLayout());
         questionsPanel.setBackground(Color.black);
         questionsPanel.setForeground(Color.WHITE);
@@ -710,7 +741,9 @@ public class QuestionPanel extends JPanel {
     class QuestionEditFrame extends JFrame {
         private QuestionData question;
         private String currentType;
-        private String[] types = {"likert", "closed", "open", "closed_button", "textArea", "sem_diff"};
+        private String[] types = {"Likert Scale", "Semantic differential", "Closed question (combo-box)", "Closed question (buttons)", "Open question", "Open question with text area"};
+        private HashMap<String, String> translations;
+        private HashMap<String, String> translationsRev;
         private JComboBox typeCombo;
         private int pos;
         private JPanel editPanel;
@@ -719,7 +752,22 @@ public class QuestionPanel extends JPanel {
 
         public QuestionEditFrame(QuestionData question, int pos, QuestionPanel parent) {
             this.pos = pos;
+            this.question = question;
             this.parent = parent;
+            translations = new HashMap<String, String>();
+            translationsRev = new HashMap<String, String>();
+            translations.put("Likert Scale", "likert");
+            translations.put("Closed question (combo-box)", "closed_combo");
+            translations.put("Open question", "open");
+            translations.put("Closed question (buttons)", "closed_button");
+            translations.put("Semantic differential", "sem_diff");
+            translations.put("Open question with text area", "textArea");
+            translationsRev.put("likert", "Likert Scale");
+            translationsRev.put("closed_combo", "Closed question (combo-box)");
+            translationsRev.put("open", "Open question");
+            translationsRev.put("closed_button", "Closed question (buttons)");
+            translationsRev.put("sem_diff", "Semantic differential");
+            translationsRev.put("textArea", "Open question with text area");
             JPanel setTypePanel = new JPanel();
             editPanel = new JPanel();
             JLabel typeLabel = new JLabel("Type of question: ");
@@ -728,30 +776,38 @@ public class QuestionPanel extends JPanel {
             if (question != null) {
                 currentType = question.getType();
                 System.out.println("Current type = " + currentType);
-                typeCombo.setSelectedItem(currentType);
+
+            } else {
+                typeCombo.setSelectedItem(translationsRev.get("open")); //Default to open question for new Question.
+                currentType = "open";
+                //   typeCombo.setSelectedItem(currentType);
                 changeQuestionEditor();
+                revalidate();
+                repaint();
             }
+
+
             setTypePanel.add(typeCombo);
             JButton setTypeButton = new JButton("Set question type");
             setTypeButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
-                    currentType = typeCombo.getSelectedItem().toString();
+                    currentType = translations.get(typeCombo.getSelectedItem().toString());
                     changeQuestionEditor();
+
                 }
             });
             setTypePanel.add(setTypeButton);
 
-            this.question = question;
 
-            this.setSize(new Dimension(400, 400));
+            //  this.setMinimumSize(new Dimension(400, 400));
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
             mainPanel.add(setTypePanel);
 
-            JPanel questionPanel = new JPanel();
-            questionPanel.setPreferredSize(new Dimension(400, 400));
-            questionPanel.add(new JLabel(question.getQuestion()));
+//            JPanel questionPanel = new JPanel();
+            //   questionPanel.setPreferredSize(new Dimension(600, 800));
+            //          questionPanel.add(new JLabel(question.getQuestion()));
             // mainPanel.add(questionPanel);
             JPanel buttonPanel = new JPanel();
             JButton okButton = new JButton("Ok");
@@ -760,43 +816,205 @@ public class QuestionPanel extends JPanel {
                 public void actionPerformed(ActionEvent actionEvent) {
                     if (qEditor.validated()) {
                         changeQuestion();
+                        closeAction();
                     }
                 }
             });
             buttonPanel.add(okButton);
+            JButton cancelButton = new JButton("Cancel");
+            cancelButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    closeAction();
+                }
+            });
+            buttonPanel.add(cancelButton);
             mainPanel.add(editPanel);
             mainPanel.add(buttonPanel);
             this.getContentPane().add(mainPanel);
+            this.pack();
+        }
+
+        private void closeAction() {
+            this.dispose();
         }
 
         private void changeQuestionEditor() {
             editPanel.removeAll();
-            if (currentType.equalsIgnoreCase("closed")) {
 
+            if (currentType.equalsIgnoreCase("closed_combo") || currentType.equalsIgnoreCase("closed_button")) {
+                ClosedPanel p = new ClosedPanel(question, currentType);
+                editPanel.removeAll();
+                editPanel.add(p);
+                qEditor = p;
+                revalidate();
             } else if (currentType.equalsIgnoreCase("open")) {
                 OpenPanel p = new OpenPanel(question);
                 editPanel.removeAll();
                 editPanel.add(p);
                 qEditor = p;
-                revalidate();
-            } else if (currentType.equalsIgnoreCase("closed_button")) {
-
             } else if (currentType.equalsIgnoreCase("textArea")) {
                 TextAreaQPanel p = new TextAreaQPanel(question);
                 editPanel.add(p);
                 qEditor = p;
 
-            } else if (currentType.equalsIgnoreCase("semDiff") || currentType.equalsIgnoreCase("likert")) {
-                LikertDiffPanel p = new LikertDiffPanel(question);
+            } else if (currentType.equalsIgnoreCase("sem_diff") || currentType.equalsIgnoreCase("likert")) {
+                LikertDiffPanel p = new LikertDiffPanel(question, currentType);
                 editPanel.add(p);
                 qEditor = p;
 
             }
             revalidate();
+            repaint();
+            pack();
         }
 
         private void changeQuestion() {
             parent.changeQuestion(qEditor.getQuestion(), pos);
+        }
+
+
+        private class ClosedPanel extends JPanel implements QuestionEditor {
+            private JTextField question, qLabel, noChoicesInput;
+            private JCheckBox required;
+            private int noChoices = 15;
+            ArrayList<JTextField> allChoices;
+            private ArrayList<String> currentChoices;
+            private String type;
+            private JPanel optionsPanel;
+
+            public ClosedPanel(QuestionData original, String type) {
+                this.type = type;
+                this.setLayout(new SpringLayout());
+                this.add(new JLabel("Question: "));
+                allChoices = new ArrayList<JTextField>();
+                currentChoices = new ArrayList<String>();
+                optionsPanel = new JPanel(new SpringLayout());
+                question = new JTextField();
+                question.setPreferredSize(new Dimension(250, 20));
+                this.add(question);
+                //    JButton choicesButton = new JButton("Set number of choices: ");
+                //    this.add(choicesButton);
+                //    choicesButton.addActionListener(new ActionListener() {
+                //       @Override
+                //      public void actionPerformed(ActionEvent actionEvent) {
+                //           changeChoicesAction();
+                //         }
+                //     });
+                //      noChoicesInput = new JTextField();
+                //    noChoicesInput.setPreferredSize(new Dimension(40, 20));
+                //   noChoicesInput.setText(String.valueOf(noChoices));
+                //    this.add(noChoicesInput);
+                this.add(new JLabel("Choices: "));
+                this.add(optionsPanel);
+                this.add(new JLabel("Label for analysis: "));
+                qLabel = new JTextField();
+                this.add(qLabel);
+                JLabel reqLabel = new JLabel("Required: ");
+                this.add(reqLabel);
+                required = new JCheckBox();
+                required.setSelected(true);
+                this.add(required);
+                if (original != null) {
+                    question.setText(original.getQuestion());
+                    qLabel.setText(original.getQuestion());
+                    required.setSelected(original.isRequired());
+                    //    if (original.getOptions().size() > 0) {
+                    //       noChoicesInput.setText(String.valueOf(original.getOptions().size()));
+                    //    }
+                    //   noChoices = original.getOptions().size();
+                    currentChoices = original.getOptions();
+                    setChoices(optionsPanel);
+                    addCurrentChoices();
+                }
+                SpringUtilities.makeCompactGrid(this,
+                        4, 2, //rows, cols
+                        6, 6,        //initX, initY
+                        6, 6);       //xPad, yPad
+
+                setChoices(optionsPanel);
+                addCurrentChoices();
+                revalidate();
+                repaint();
+            }
+
+            //Fill with the current option values after a change or when filled for the first time and original question has values.
+            private void addCurrentChoices() {
+                int smallest = allChoices.size();
+                if (smallest < currentChoices.size()) {
+                    smallest = currentChoices.size();
+                }
+                for (int x = 0; x < smallest; x++) {
+                    if (x < currentChoices.size() && x < allChoices.size()) {
+                        allChoices.get(x).setText(currentChoices.get(x));
+                    }
+                }
+            }
+
+            private void setChoices(JPanel contentPanel) {
+
+                contentPanel.removeAll();
+                allChoices.clear();
+                for (int x = 0; x < noChoices; x++) {
+                    JLabel label = new JLabel("Option " + x + ": ");
+                    contentPanel.add(label);
+                    JTextField option = new JTextField();
+                    option.setPreferredSize(new Dimension(250, 20));
+                    option.setMinimumSize(new Dimension(250, 20));
+                    option.setMaximumSize(new Dimension(250, 20));
+                    contentPanel.add(option);
+                    allChoices.add(option);
+                }
+                contentPanel.revalidate();
+                SpringUtilities.makeCompactGrid(contentPanel,
+                        noChoices, 2, //rows, cols
+                        6, 6,        //initX, initY
+                        6, 6);       //xPad, yPad
+            }
+
+            private void changeChoicesAction() {
+                //First save the current options
+                currentChoices.clear();
+                for (JTextField t : allChoices) {
+                    currentChoices.add(t.getText());
+                }
+                boolean succes = true;
+                int newChoices = 0;
+                try {
+                    newChoices = Integer.parseInt(noChoicesInput.getText());
+                } catch (Exception e) {
+                    succes = false;
+                }
+                if (succes && newChoices > 0) {
+                    noChoices = newChoices;
+                }
+                setChoices(optionsPanel);
+                addCurrentChoices();
+            }
+
+            @Override
+            public QuestionData getQuestion() {
+                QuestionData questionData = new QuestionData(type);
+                System.out.println("Adding "+type);
+                questionData.setKey(qLabel.getText());
+                questionData.setQuestion(question.getText());
+                questionData.setRequired(required.isSelected());
+                currentChoices.clear();
+                for (JTextField t : allChoices) {
+                    currentChoices.add(t.getText());
+                }
+                for (String s : currentChoices) {
+                    if (s.length() > 0) {
+                        questionData.addOptions(s);
+                    }
+                }
+                return questionData;
+            }
+
+            @Override
+            public boolean validated() {
+                return true;
+            }
         }
 
 
@@ -846,14 +1064,18 @@ public class QuestionPanel extends JPanel {
             }
         }
 
+        /**
+         * Class used for the likert scale and the semantic differential scale. Basis properties are the same for both.
+         */
         private class LikertDiffPanel extends JPanel implements QuestionEditor {
             private JTextField qLabel;
             private JTextField question, left, right, size;
             private String type;
             private JCheckBox required;
 
-            public LikertDiffPanel(QuestionData original) {
-                this.type = original.getType();
+            public LikertDiffPanel(QuestionData original, String type) {
+                    this.type = type;
+
                 this.setLayout(new SpringLayout());
 
                 this.add(new JLabel("Question: "));
@@ -936,6 +1158,7 @@ public class QuestionPanel extends JPanel {
                 this.setLayout(new SpringLayout());
                 this.add(new JLabel("Question: "));
                 question = new JTextField();
+                question.setPreferredSize(new Dimension(250, 20));
                 this.add(question);
                 this.add(new JLabel("Label for analysis: "));
                 qLabel = new JTextField();
